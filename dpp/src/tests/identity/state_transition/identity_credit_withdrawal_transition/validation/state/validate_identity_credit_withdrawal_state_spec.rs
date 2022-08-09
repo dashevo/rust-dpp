aaa14d11224a7e538cdca94f3cf7617dc9d364b2
Djavid Gabibiyan <djavidoff@gmail.com>
2022-08-09 14:22:35 +0500

    chore: rest of the state tests

--- dpp/src/tests/identity/state_transition/identity_credit_withdrawal_transition/validation/state/validate_identity_credit_withdrawal_state_spec.rs
+++ dpp/src/tests/identity/state_transition/identity_credit_withdrawal_transition/validation/state/validate_identity_credit_withdrawal_state_spec.rs
@@ -8,18 +8,27 @@ use crate::{
 
 pub fn setup_test<SR: StateRepositoryLike>(
     state_repository_mock: SR,
+    amount_option: Option<u64>,
 ) -> (
     IdentityCreditWithdrawalTransition,
     IdentityCreditWithdrawalTransitionValidator<SR>,
 ) {
+    let mut state_transition = IdentityCreditWithdrawalTransition::default();
+
+    if let Some(amount) = amount_option {
+        state_transition.amount = amount;
+    }
+
     (
-        IdentityCreditWithdrawalTransition::default(),
+        state_transition,
         IdentityCreditWithdrawalTransitionValidator::new(state_repository_mock),
     )
 }
 
 #[cfg(test)]
 mod validate_identity_credit_withdrawal_transition_state_factory {
+    use crate::assert_consensus_errors;
+    use crate::consensus::ConsensusError;
     use crate::prelude::Identity;
 
     use super::*;
@@ -32,10 +41,66 @@ mod validate_identity_credit_withdrawal_transition_state_factory {
             .expect_fetch_identity::<Identity>()
             .returning(|_| anyhow::Ok(None));
 
-        let (state_transition, validator) = setup_test(state_repository);
+        let (state_transition, validator) = setup_test(state_repository, None);
 
         let result = validator
             .validate_identity_credit_withdrawal_transition_state(&state_transition)
             .await;
+
+        assert_consensus_errors!(result, ConsensusError::BasicError, 1);
+
+        let error = result.first_error().unwrap();
+
+        assert_eq!(error.code(), 2000);
+    }
+
+    #[tokio::test]
+    async fn should_return_invalid_result_if_identity_have_not_enough_balance() {
+        let mut state_repository = MockStateRepositoryLike::default();
+
+        state_repository
+            .expect_fetch_identity::<Identity>()
+            .returning(|_| {
+                let mut identity = Identity::default();
+
+                identity = identity.set_balance(10);
+
+                anyhow::Ok(Some(identity))
+            });
+
+        let (state_transition, validator) = setup_test(state_repository, Some(42));
+
+        let result = validator
+            .validate_identity_credit_withdrawal_transition_state(&state_transition)
+            .await;
+
+        assert_consensus_errors!(result, ConsensusError::IdentityInsufficientBalanceError, 1);
+
+        let error = result.first_error().unwrap();
+
+        assert_eq!(error.code(), 4023);
+    }
+
+    #[tokio::test]
+    async fn should_return_valid_result() {
+        let mut state_repository = MockStateRepositoryLike::default();
+
+        state_repository
+            .expect_fetch_identity::<Identity>()
+            .returning(|_| {
+                let mut identity = Identity::default();
+
+                identity = identity.set_balance(10);
+
+                anyhow::Ok(Some(identity))
+            });
+
+        let (state_transition, validator) = setup_test(state_repository, Some(5));
+
+        let result = validator
+            .validate_identity_credit_withdrawal_transition_state(&state_transition)
+            .await;
+
+        assert_eq!(result.is_valid(), true);
     }
 }
